3e3cf50a66d4530620227d59fcc0b86fd623caec
Pengqi Liu <lpq0105@gmail.com>
2013-04-24 14:14:35 -0400

    Final for NB

--- src/naivebayes/TestNB.java
+++ src/naivebayes/TestNB.java
@@ -3,11 +3,11 @@ package naivebayes;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Vector;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import tokenweight.NormalizedTokenizerFactory;
@@ -19,7 +19,7 @@ import com.aliasi.tokenizer.Tokenizer;
 import com.aliasi.tokenizer.TokenizerFactory;
 
 
-public class TestNBUsingRequest {
+public class TestNB {
 	//The Weight of Different Token
 	private HashMap<String,Double> weight = new HashMap<String,Double>();
 	//The number of training instances of class Y
@@ -31,9 +31,6 @@ public class TestNBUsingRequest {
 	//Number of instances
 	private int totalY;
 	private int totalVoca;
-	private int totalTest = 0;
-	private double totalRight = 0;
-	private double precision = 0;
 	
 	public void processRecord(String record){
 		String[] mapping = record.split("\t");
@@ -59,6 +56,7 @@ public class TestNBUsingRequest {
 		return;
 	}
 	public void processTestData(String file) throws IOException{
+		int count = 0;
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String tag = null;
 		String l = null;
@@ -66,41 +64,51 @@ public class TestNBUsingRequest {
 		int right = 0;
 		int total = 0;
 		while(br.ready()){
-			String doc = br.readLine();		
+			count++;
+			String doc = br.readLine();	
 			String[] attr = doc.split("\t");
 			String hashTag = attr[0];
 			String label = attr[1];
 			String jsonLine = attr[2];
-	    	JSONObject json = new JSONObject(jsonLine);
-	        String text = json.getString("text");
+			String text = null;
+			try{
+				JSONObject json = new JSONObject(jsonLine);
+		        text = json.getString("text");
+			}catch(JSONException je){
+				text = "";
+			}
 	        if(tag!=null&&!hashTag.equals(tag)){
 	        	String c = testDoc(sb.toString().trim());
+	        	System.out.println(tag+" "+c+" "+l);
+	        	sb = new StringBuffer();
 	        	if(c.equals(l)) right++;
 	        	total++;
-	        }else{
-	        	tag = hashTag;
-	        	l = label;
-	        	sb.append(text+" ");
-	        } 
+	        	count = 0;
+	        }
+	        tag = hashTag;
+	        l = label;
+	        sb.append(text+" ");
 		}
     	String c = testDoc(sb.toString().trim());
     	if(c.equals(l)) right++;
     	total++;
 		br.close();
-		System.out.println("Precision:"+(double)right/total);
+		System.out.println("Precision:"+right+"/"+total+"="+(double)right/total);
 	}
 	public String testDoc(String text){
 		if(text!=null){
 			Vector<String> tokens = tokenizeDoc(text);
 			Iterator<String> it = countY.keySet().iterator();
-			double p = -1000000;
+			double p = Double.NEGATIVE_INFINITY;
 			String maxClass = "";
 			while(it.hasNext()){
 				String c = (String)it.next();
 				double prob = Math.log((double)(1+countY.get(c))/(double)(countY.size()+totalY));
+				System.out.println(prob);
 				for(String s:tokens){
 					prob = prob + calProb(c,s);
 				}
+				System.out.println(prob);
 				if(prob>p){
 					p = prob;
 					maxClass = c;
@@ -132,14 +140,18 @@ public class TestNBUsingRequest {
 		return tokens;
 	}
 	public double calProb(String clas,String token){
-		int count = 0;
+		double count = 0;
+		double time = 1;
 		if(countYToken.containsKey(token+":"+clas)){
 			count = countYToken.get(token+":"+clas);
+			//time = 1+100000*weight.get(token);
+			//System.out.println(totalVoca);
+			count = count * time;
 		}
 		return Math.log((double)(count+1)/(double)(countToken.get(clas)+totalVoca));
 	}
 	public static void main(String[] args) throws IOException{
-		TestNBUsingRequest nb = new TestNBUsingRequest();
+		TestNB nb = new TestNB();
 		//Read Training Model
 		BufferedReader br = new BufferedReader(new FileReader(args[0]));
 		while(br.ready()){
@@ -148,19 +160,23 @@ public class TestNBUsingRequest {
 		}
 		br.close();
 		//Read Weight Data
-		for(int i=0;i<6;i++){
-			br = new BufferedReader(new FileReader(args[1]+i));
+		
+		for(int i=1;i<7;i++){
+			br = new BufferedReader(new FileReader(args[2]+i));
 			while(br.ready()){
 				String record = br.readLine();
 				String[] count = record.split("\t");
-				nb.weight.put(count[0]+":"+i,Double.parseDouble(count[1]));
+				if(nb.weight.containsKey(count[0])){
+					nb.weight.put(count[0],Double.parseDouble(count[1])+nb.weight.get(count[0]));
+				}else{
+					nb.weight.put(count[0],Double.parseDouble(count[1]));
+				}
+				
 			}
 			br.close();
 		}
-		//Read Test Data
-		nb.processTestData(args[2]);
 		
-		nb.precision = nb.totalRight/nb.totalTest;
-		System.out.println("Percent Correct: "+nb.precision);
+		//Read Test Data
+		nb.processTestData(args[1]);
 	}
 }
